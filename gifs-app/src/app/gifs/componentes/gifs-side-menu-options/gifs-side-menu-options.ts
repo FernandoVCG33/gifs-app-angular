632d8dbdc8e3a34c5f9a16944d8a1d21afe32da3
Fernando Catacora <fernando.catacora@ucb.edu.bo>
2025-12-16 14:14:05 -0400

    Primeras bases de la app 3

--- gifs-app/src/app/gifs/componentes/gifs-side-menu-options/gifs-side-menu-options.ts
+++ gifs-app/src/app/gifs/componentes/gifs-side-menu-options/gifs-side-menu-options.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 
+interface MenuOption{
+  label:string;
+  subLabel:string;
+  route:string;
+  icon:string;
+}
+
 @Component({
   selector: 'gifs-gifs-side-menu-options',
   imports: [],
   templateUrl: './gifs-side-menu-options.html',
   })
-export  class GifsSideMenuOptions {
-
+export class GifsSideMenuOptions {
+    menuOptions: MenuOption[]=[
+      {
+        icon:'fa-solid fa-chart-line',
+        label: 'Trending',
+        subLabel:'Gifs Populares',
+        route:'/dashboard/trending',
+      },
+      {
+        icon:'fa-solid fa-magnifying-glass',
+        label: 'Buscardor',
+        subLabel:'Buscar gifs',
+        route:'/dashboard/search',
+      }
+    ];
 }
