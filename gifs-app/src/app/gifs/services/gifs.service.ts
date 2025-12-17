cab6528afa246286b89d831e2d3dc2e1396992a7
Fernando Catacora <fernando.catacora@ucb.edu.bo>
2025-12-17 15:02:02 -0400

    Interfaz agregada services y arregos nuevos

--- gifs-app/src/app/gifs/services/gifs.service.ts
+++ gifs-app/src/app/gifs/services/gifs.service.ts
@@ -5,12 +5,18 @@ import {environment} from '@enviroments/environment.development';
 @Injectable({providedIn: 'root'})
 export class NameService {
   private http=inject(HttpClient);
+
+  constructor() {
+    this.loadTrendingGigs();
+  }
   loadTrendingGigs(){
     this.http.get(`${environment.giphyUrl}/gifs/trending`,{
       params:{
         api_key:environment.gifapiKey,
         limit :20,
       },
-    });
+    }).subscribe( (resp) =>{
+        console.log({resp});
+    } )
   }
 }
