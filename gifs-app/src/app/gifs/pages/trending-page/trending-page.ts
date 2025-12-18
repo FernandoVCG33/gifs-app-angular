986b1fefbe6816559532ec3246e8db497b68be81
Fernando Catacora <fernando.catacora@ucb.edu.bo>
2025-12-18 11:10:28 -0400

    Nueva tarea

--- gifs-app/src/app/gifs/pages/trending-page/trending-page.ts
+++ gifs-app/src/app/gifs/pages/trending-page/trending-page.ts
@@ -7,10 +7,8 @@ import {
   inject,
   viewChild
 } from '@angular/core';
-import {GifList} from '../../componentes/list/gif-list/gif-list';
+//import {GifList} from '../../componentes/list/gif-list/gif-list';
 import {GifService} from '../../services/gifs.service';
-
-
 @Component({
   selector: 'app-trending-page',
   templateUrl: './trending-page.html',
@@ -21,9 +19,14 @@ import {GifService} from '../../services/gifs.service';
 export default class TrendingPage {
   //decalrar en una nueva variable el array
     gifsService= inject(GifService);
-    scrollDivRef= viewChild<ElementRef>('gropupDiv');
+    scrollDivRef= viewChild<ElementRef<HTMLDivElement>>('gropupDiv');
     onScroll(event: Event){
-        const scrollDiv=this.scrollDivRef()?.nativeElement;
-        //console.log(scrollDiv);
+      const scrollDiv=this.scrollDivRef()?.nativeElement;
+      if(!scrollDiv) return;
+        const scrolTop=scrollDiv.scrollTop;
+        const clientheight = scrollDiv.clientHeight;
+        const scrollHeight=scrollDiv.scrollHeight;
+        const isABoton= scrolTop+ clientheight + 300 >= scrollHeight;
+        console.log({isABoton});
     }
 }
